2875006bcd4212f458eae66232d1358ba0c6b9c2
evquiros <valverde.quiros.emilio@gmail.com>
2024-09-09 22:22:19 -0600

    Complete 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let phrase = "";
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+        for (let i = 1; i <= num; i++) {
+            phrase += string
+        }
+        return phrase
+    }
 };
+
+let string = "";
+let num = 10;
+
+repeatString(string, num);
 
 // Do not edit below this line
 module.exports = repeatString;
