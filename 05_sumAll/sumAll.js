0701cd83cea9ff952784e5b632cde6a62082dd33
evquiros <valverde.quiros.emilio@gmail.com>
2024-09-10 00:24:50 -0600

    Complete 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let result = 0
 
+    if (num1 == NaN || num2 == NaN || !Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    } else if(num1 > num2){
+        for (let i = num2; i <= num1; i++) {
+            result += i;
+        }
+
+        return result
+    } else {
+        for (let i = num1; i <= num2; i++) {
+            result += i;
+        }
+
+        return result
+    }
 };
 
 // Do not edit below this line
