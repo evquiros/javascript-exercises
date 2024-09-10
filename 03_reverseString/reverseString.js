8909e30ad6c892c0c6c522f6c1b229f333725a4c
evquiros <valverde.quiros.emilio@gmail.com>
2024-09-09 22:58:33 -0600

    Complete 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("")
+    let reverseString = stringArray.reverse()
+    return reverseString.join("")
 };
+
+let string = '';
+
+reverseString(string);
 
 // Do not edit below this line
 module.exports = reverseString;
