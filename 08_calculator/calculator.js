2e745c1803e6daf155f875cc4e047f071d3a5ff5
evquiros <valverde.quiros.emilio@gmail.com>
2024-09-14 17:15:11 -0600

    Complete 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, accum) => total + accum, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, accum) => total * accum)
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return Math.pow(num, power)
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  let total = 1
+
+  if (factorial == 0) {
+    return 1
+  }
+
+  for (let i = 1; i <= factorial; i++) {
+    total *= i
+  }
+
+  return total
 };
 
 // Do not edit below this line
