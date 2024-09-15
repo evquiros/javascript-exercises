e01144f0bcbed50c05e9f97a812e0c5a85c49ba8
evquiros <valverde.quiros.emilio@gmail.com>
2024-09-14 22:30:43 -0600

    Complete 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
 
+  let currentAge = getAge(person.yearOfDeath, person.yearOfBirth)
+
+  let oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth)
+
+  return oldestAge > currentAge ? oldest : person
+  })
 };
+
+const getAge = function(death, birth){
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
